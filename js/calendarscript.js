af3960ca863f14a1bd66d74668432d9cc4dc3f02
charlie-graphy <168528908+charlie-graphy@users.noreply.github.com>
2024-07-22 10:14:23 +0900

    Add files via upload

--- js/calendarscript.js
+++ js/calendarscript.js
@@ -722,21 +722,21 @@
 				},
 				{
 					title: '등등곡',
-					description: '김영운 役 | 김지철<br>최윤 役 | 안지환<br>초 役 | 김서환<br>정진명 役 | 김경록<br>이경신 役 | 임태현',
+					description: '<p class="event">등등회 엽전 증정 & 커튼콜 데이</p>김영운 役 | 김지철<br>최윤 役 | 안지환<br>초 役 | 김서환<br>정진명 役 | 김경록<br>이경신 役 | 임태현',
 					start: '2024-07-31 16:00',
 					className: 'bg-red',
 					allDay: false
 				},
 				{
 					title: '등등곡',
-					description: '김영운 役 | 김재범<br>최윤 役 | 안지환<br>초 役 | 박준휘<br>정진명 役 | 김경록<br>이경신 役 | 황두현',
+					description: '<p class="event">등등회 엽전 증정 & 커튼콜 데이</p>김영운 役 | 김재범<br>최윤 役 | 안지환<br>초 役 | 박준휘<br>정진명 役 | 김경록<br>이경신 役 | 황두현',
 					start: '2024-08-01 20:00',
 					className: 'bg-red',
 					allDay: false
 				},
 				{
 					title: '등등곡',
-					description: '김영운 役 | 유승현<br>최윤 役 | 안지환<br>초 役 | 박준휘<br>정진명 役 | 김경록<br>이경신 役 | 황두현',
+					description: '<p class="event">등등회 엽전 증정 & 커튼콜 데이</p>김영운 役 | 유승현<br>최윤 役 | 안지환<br>초 役 | 박준휘<br>정진명 役 | 김경록<br>이경신 役 | 황두현',
 					start: '2024-08-03 19:00',
 					className: 'bg-red',
 					allDay: false
