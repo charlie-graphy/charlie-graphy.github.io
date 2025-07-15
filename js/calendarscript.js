3f1a637df70bd0f9b3062afb98f00214a29657e9
Su Yeon <168528908+charlie-graphy@users.noreply.github.com>
2025-07-15 10:37:16 +0900

    Add files via upload

--- js/calendarscript.js
+++ js/calendarscript.js
@@ -1594,6 +1594,34 @@
 					start: '2025-08-24 18:00',
 					className: 'bg-gray',
 					allDay: false
+				},
+				{
+					title: '렛미플라이 - 홍콩',
+					description: '노인 남원 役 | 김태한<br>선희 役 | ?<br>청년 남원 役 | 안지환<br>정분 役 | ?',
+					start: '2025-11-21 20:00',
+					className: 'bg-blue',
+					allDay: false
+				},
+				{
+					title: '렛미플라이 - 홍콩',
+					description: '노인 남원 役 | 김태한<br>선희 役 | ?<br>청년 남원 役 | 안지환<br>정분 役 | ?',
+					start: '2025-11-22 15:00',
+					className: 'bg-blue',
+					allDay: false
+				},
+				{
+					title: '렛미플라이 - 홍콩',
+					description: '노인 남원 役 | 김태한<br>선희 役 | ?<br>청년 남원 役 | 안지환<br>정분 役 | ?',
+					start: '2025-11-22 19:30',
+					className: 'bg-blue',
+					allDay: false
+				},
+				{
+					title: '렛미플라이 - 홍콩',
+					description: '노인 남원 役 | 김태한<br>선희 役 | ?<br>청년 남원 役 | 안지환<br>정분 役 | ?',
+					start: '2025-11-23 15:00',
+					className: 'bg-blue',
+					allDay: false
 				}
 			],
 			eventClick: function(event, jsEvent, view) {
